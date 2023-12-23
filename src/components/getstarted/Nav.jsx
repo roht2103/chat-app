6aa033a945345495c378f9f989f506b1b6af2be3
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2023-12-23 23:20:32 +0530

    added styling to nav and given name to app

--- src/components/getstarted/Nav.jsx
+++ src/components/getstarted/Nav.jsx
@@ -1,11 +1,13 @@
 import { useNavigate } from "react-router-dom";
+import messageicon from "../../assets/message-icon.svg";
 const NavComponents = () => {
   const navigate = useNavigate();
   return (
     <>
       <div className="nav">
         <span>
-          <h1>logo</h1>
+          <img src={messageicon} alt="message-icon" height="32px" />
+          <h1>ZenChat</h1>
         </span>
         <span className="links">
           <button onClick={() => navigate("login-page")}>Log In</button>
