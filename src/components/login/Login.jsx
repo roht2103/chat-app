333e6f0017ead4eec5fde4f9cca319b2e7cb1909
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2023-12-24 03:48:16 +0530

    created and styled login page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,39 @@
+import { useNavigate } from "react-router-dom";
+import signUpImg from "../../assets/signUp.svg";
+import avatar from "../../assets/avatar.svg";
 const LoginComponent = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <h1>login</h1>
+      <div className="body">
+        <section>
+          <div>
+            <img src={signUpImg} alt="signUp.svg" />
+          </div>
+          <div className="login-form">
+            <h1>Get Started</h1>
+            <form>
+              <input type="email" id="email" placeholder="email" required />
+              <input
+                type="password"
+                id="pass"
+                placeholder="Password"
+                required
+              />
+              <input type="submit" id="submit-btn" value="Log In" />
+              <p>
+                Don't have an Account..?
+                <button
+                  className="signInBtn"
+                  onClick={() => navigate("/signUp-page")}
+                >
+                  Sign Up
+                </button>
+              </p>
+            </form>
+          </div>
+        </section>
+      </div>
     </>
   );
 };
