f5325ec1dbb98d1318af38e1de7aa1ca1ee2ccd7
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2023-12-28 18:17:11 +0530

    added last message section

--- src/components/userChatWindow/ChatUser.jsx
+++ src/components/userChatWindow/ChatUser.jsx
@@ -1,10 +1,11 @@
 import "./style.css";
-export const ChatUser = ({ profileUrl, name }) => {
+export const ChatUser = ({ profileUrl, name, lastMsg }) => {
   return (
     <div className="userChat">
       <img src={profileUrl} alt="user-img" />
       <div className="userChatInfo">
         <span>{name}</span>
+        <p>{lastMsg}</p>
       </div>
     </div>
   );
--- src/components/userChatWindow/Chats.jsx
+++ src/components/userChatWindow/Chats.jsx
@@ -6,34 +6,42 @@ export const Chats = () => {
       <ChatUser
         profileUrl="https://images.unsplash.com/photo-1610088441520-4352457e7095?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG1lbnxlbnwwfHwwfHx8MA%3D%3D"
         name="adam"
+        lastMsg="hello"
       />
       <ChatUser
         profileUrl="https://images.unsplash.com/photo-1488371934083-edb7857977df?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fG1lbnxlbnwwfHwwfHx8MA%3D%3D"
         name="Albert"
+        lastMsg="Goog"
       />
       <ChatUser
         profileUrl="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bWVufGVufDB8fDB8fHww"
         name="George"
+        lastMsg="Bye"
       />
       <ChatUser
         profileUrl="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bWVufGVufDB8fDB8fHww"
         name="George"
+        lastMsg="How are you?"
       />
       <ChatUser
         profileUrl="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bWVufGVufDB8fDB8fHww"
         name="George"
+        lastMsg="Good morning"
       />
       <ChatUser
         profileUrl="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bWVufGVufDB8fDB8fHww"
         name="George"
+        lastMsg="homework?"
       />
       <ChatUser
         profileUrl="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bWVufGVufDB8fDB8fHww"
         name="George"
+        lastMsg="he said that"
       />
       <ChatUser
         profileUrl="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bWVufGVufDB8fDB8fHww"
         name="George"
+        lastMsg="Happy diwali"
       />
     </div>
   );
