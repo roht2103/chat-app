6ea2c1e434297804353111fca0b8bbd3fd45dd5b
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2024-01-04 00:50:44 +0530

    added back button to chat

--- src/components/userChatWindow/Chat.jsx
+++ src/components/userChatWindow/Chat.jsx
@@ -3,6 +3,7 @@ import { Messages } from "./Messages.jsx";
 import { Input } from "./Input.jsx";
 import videoIcon from "../../assets/video-solid.svg";
 import more from "../../assets/more.svg";
+import back from "../../assets/back.svg";
 export const Chat = () => {
   return (
     <div className="chat">
@@ -15,6 +16,7 @@ export const Chat = () => {
             cursor: "pointer",
           }}
         >
+          <img height="35px" className="back" src={back} alt="back.svg" />
           <img
             className="userImg"
             src="https://images.unsplash.com/photo-1610088441520-4352457e7095?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG1lbnxlbnwwfHwwfHx8MA%3D%3D"
