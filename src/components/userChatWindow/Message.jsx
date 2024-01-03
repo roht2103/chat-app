f81df767732f45b9150aa0c9344b554e35905113
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2024-01-03 11:36:12 +0530

    created message component

--- src/components/userChatWindow/Message.jsx
+++ src/components/userChatWindow/Message.jsx
@@ -1,8 +1,24 @@
 import "./style.css";
-export const Message = () => {
+export const Message = ({ imgSrc, msg }) => {
+  if (imgSrc) {
+    console.log(true);
+  }
   return (
     <div className="message">
-      <p>Message</p>
+      <div className="messageInfo">
+        <img
+          className="userImg"
+          src="https://images.unsplash.com/photo-1610088441520-4352457e7095?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG1lbnxlbnwwfHwwfHx8MA%3D%3D"
+          alt="userImg"
+        />
+        <span>Just Now</span>
+      </div>
+      <div className="messageContent">
+        <p>
+          <img className="inputImg" src={imgSrc} alt="" />
+          {msg}
+        </p>
+      </div>
     </div>
   );
 };
