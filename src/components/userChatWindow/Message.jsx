fc5436cd5cf6915ffe089299c9d47be0b032c36b
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2024-01-16 22:22:53 +0530

    styled message component

--- src/components/userChatWindow/Message.jsx
+++ src/components/userChatWindow/Message.jsx
@@ -5,27 +5,34 @@ import { ChatContext } from "../../context/ChatContext";
 export const Message = ({ message }) => {
   const { currentUser } = useContext(AuthContext);
   const { data } = useContext(ChatContext);
+  const ref = useRef();
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
   return (
-    <div
-      className={`message ${message.senderId === currentUser.uid && "owner"}`}
-    >
-      <div className="messageInfo">
-        <img
-          className="userImg"
-          src={
-            message.senderId === currentUser.uid
-              ? currentUser.photoURL
-              : data.user.photoURL
-          }
-          alt="userImg"
-        />
-        <span>Just Now</span>
-      </div>
-      <div className="messageContent">
-        <p>
-          {message.img && <img className="inputImg" src={message.img} alt="" />}
-          {message.text}
-        </p>
+    <div ref={ref}>
+      <div
+        className={`message ${message.senderId === currentUser.uid && "owner"}`}
+      >
+        <div className="messageInfo">
+          <img
+            className="userImg"
+            src={
+              message.senderId === currentUser.uid
+                ? currentUser.photoURL
+                : data.user.photoURL
+            }
+            alt="userImg"
+          />
+        </div>
+        <div className="messageContent">
+          <p>
+            {message.img && (
+              <img className="inputImg" src={message.img} alt="" />
+            )}
+            {message.text}
+          </p>
+        </div>
       </div>
     </div>
   );
