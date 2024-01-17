b8f3e9b7166130aa0b0d0fec123e718bf5aa2747
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2024-01-17 19:57:59 +0530

    Truncate lastMsg in ChatUser component

--- src/components/userChatWindow/ChatUser.jsx
+++ src/components/userChatWindow/ChatUser.jsx
@@ -1,12 +1,22 @@
 import "./style.scss";
 
 export const ChatUser = ({ profileUrl, name, lastMsg, onClick }) => {
+  const truncateText = (text, maxLength) => {
+    if (text.length <= maxLength) {
+      return text;
+    } else {
+      return text.slice(0, maxLength) + " ...";
+    }
+  };
+
+  const truncatedMsg = truncateText(lastMsg, 20);
+
   return (
     <div className="userChat" onClick={onClick}>
       <img src={profileUrl} alt="user-img" />
       <div className="userChatInfo">
         <span>{name}</span>
-        <p>{lastMsg}</p>
+        <p>{truncatedMsg}</p>
       </div>
     </div>
   );
