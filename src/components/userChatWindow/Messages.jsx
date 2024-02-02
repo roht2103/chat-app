584540f0fbf2e6dcf016b7e6af6b2d2d0894e461
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2024-02-02 15:13:08 +0530

    focus mode for messages

--- src/components/userChatWindow/Messages.jsx
+++ src/components/userChatWindow/Messages.jsx
@@ -1,22 +1,50 @@
+// Messages.jsx
 import "./style.scss";
 import { Message } from "./Message.jsx";
 import { useEffect, useContext, useState } from "react";
 import { onSnapshot } from "firebase/firestore";
 import { ChatContext } from "../../context/ChatContext.jsx";
-import { doc } from "firebase/firestore";
-import { db } from "../../firebase.js";
+import { doc, getDoc } from "firebase/firestore";
+import { db, auth } from "../../firebase.js";
+
 export const Messages = () => {
   const { data } = useContext(ChatContext);
   const [messages, setMessages] = useState([]);
+  const [isFocusMode, setFocusMode] = useState(false);
+
+  const fetchUserData = async () => {
+    const currentUser = auth.currentUser;
+    if (currentUser) {
+      const userId = currentUser.uid;
+      const userRef = doc(db, "users", userId);
+
+      try {
+        const userDoc = await getDoc(userRef);
+        if (userDoc.exists()) {
+          const userData = userDoc.data();
+          setFocusMode(userData.isFocus);
+          console.log("Is Focus Mode:", userData.isFocus);
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    }
+  };
+
   useEffect(() => {
     const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
-      doc.exists() && setMessages(doc.data().messages);
+      fetchUserData();
+
+      // Update messages only if there are messages in the chat and the focus mode is off
+      if (!isFocusMode && doc.exists()) {
+        setMessages(doc.data().messages);
+      }
     });
 
     return () => {
       unSub();
     };
-  }, [data.chatId]);
+  }, [data.chatId, isFocusMode]);
 
   return (
     <div className="messages">
